01a0629b772f5910c4d4b5441338293910c6d923
Sajjad <sajad.abedi@veriff.net>
2022-09-24 10:53:46 +0300

    Initial Commit

---
+++ DownloadAndSave/DownloadAndSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadAndSaveApp.swift
+//  DownloadAndSave
+//
+//  Created by Sajad Abedi on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct DownloadAndSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
